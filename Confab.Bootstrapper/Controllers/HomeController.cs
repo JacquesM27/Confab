6a6b9c03e07f1235fedb6f4e9ff172acd713684a
Jakub <42150609+JacquesM27@users.noreply.github.com>
2024-01-06 17:10:32 +0100

    added projects: bootstrapper, shared abstractions and infra

---
+++ Confab.Bootstrapper/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Confab.Bootstrapper.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public ActionResult Get()
+        {
+            return Ok("Confab API!");
+        }
+    }
+}
