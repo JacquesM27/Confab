32a01b80e992838645f2577e8d3dbacadfd1592c
Jakub <42150609+JacquesM27@users.noreply.github.com>
2024-02-06 20:39:18 +0100

    added unit tests for attendable event and participant

---
+++ tests/Modules/Attendances/Confab.Modules.Attendances.Tests.Unit/Entities/AttendableEvent_Attend_Tests.cs
@@ -0,0 +1,88 @@
+﻿using Confab.Modules.Attendances.Domain.Entities;
+using Confab.Modules.Attendances.Domain.Exceptions;
+using Shouldly;
+
+namespace Confab.Modules.Attendances.Tests.Unit.Entities;
+
+public class AttendableEvent_Attend_Tests
+{
+    private Attendance Act(Participant participant) => _attendableEvent.Attend(participant);
+    
+    [Fact]
+    public void given_no_slots_attend_should_fail()
+    {
+        // Arrange
+        var participant = GetParticipant();
+        
+        // Act
+        var exception = Record.Exception(() => Act(participant));
+
+        // Assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<NoFreeSlotsException>();
+        _attendableEvent.Slots.ShouldBeEmpty();
+    }
+        
+    [Fact]
+    public void given_existing_slot_with_same_participant_attend_should_fail()
+    {
+        // Arrange
+        var participant = GetParticipant();
+        _attendableEvent.AddSlots(new Slot(Guid.NewGuid(), participant.Id));
+        
+        // Act
+        var exception = Record.Exception(() => Act(participant));
+
+        // Assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<AlreadyParticipatingInEventException>();
+    }
+    
+        
+    [Fact]
+    public void given_no_free_slots_attend_should_fail()
+    {
+        // Arrange
+        var participant1 = GetParticipant();
+        var participant2 = GetParticipant();
+        _attendableEvent.AddSlots(new Slot(Guid.NewGuid(), participant1.Id));
+        
+        // Act
+        var exception = Record.Exception(() => Act(participant2));
+
+        // Assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<NoFreeSlotsException>();
+    }
+
+        
+    [Fact]
+    public void given_free_slot_attend_should_succeed()
+    {
+        // Arrange
+        var participant = GetParticipant();
+        var slot = new Slot(Guid.NewGuid());
+        _attendableEvent.AddSlots(slot);
+        
+        // Act
+        var attendance = Act(participant);
+
+        // Assert
+        attendance.ShouldNotBeNull();
+        attendance.ParticipantId.ShouldBe(participant.Id);
+        attendance.SlotId.ShouldBe(slot.Id);
+    }
+    
+    private readonly Guid _conferenceId = Guid.NewGuid();
+    private readonly AttendableEvent _attendableEvent;
+    
+    // Setup
+    public AttendableEvent_Attend_Tests()
+    {
+        _attendableEvent = new AttendableEvent(Guid.NewGuid(), _conferenceId, 
+            new DateTime(2024,2,6, 9, 0, 0), new DateTime(2024,2,6, 10, 0, 0));
+    }
+
+    private Participant GetParticipant()
+        => new (Guid.NewGuid(), _conferenceId, Guid.NewGuid());
+}
---
+++ tests/Modules/Attendances/Confab.Modules.Attendances.Tests.Unit/Entities/Participant_Attend_Tests.cs
@@ -0,0 +1,89 @@
+﻿using Confab.Modules.Attendances.Domain.Entities;
+using Confab.Modules.Attendances.Domain.Exceptions;
+using Shouldly;
+
+namespace Confab.Modules.Attendances.Tests.Unit.Entities;
+
+public class Participant_Attend_Tests
+{
+    private void Act(Attendance attendance) => _participant.Attend(attendance);
+
+    [Fact]
+    public void given_the_same_agendaItem_attend_should_fail()
+    {
+        // Arrange
+        var attendance1 = new Attendance(Guid.NewGuid(), _agendaItemId, Guid.NewGuid(), _participant.Id, GetDate(9), GetDate(10));
+        var attendance2 = new Attendance(Guid.NewGuid(), _agendaItemId, Guid.NewGuid(), _participant.Id, GetDate(9), GetDate(10));
+        _participant.Attend(attendance1);
+
+        // Act
+        var exception = Record.Exception(() => Act(attendance2));
+
+        // Asset
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<AlreadyParticipatingInEventException>();
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetCollidingDates))]
+    public void given_attendance_with_colliding_time_attend_should_fail(DateTime from, DateTime to)
+    {
+        // Arrange
+        var attendance1 = new Attendance(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), _participant.Id, from, to);
+        var attendance2 = new Attendance(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), GetDate(9,30), GetDate(11));
+        _participant.Attend(attendance1);
+
+        // Act
+        var exception = Record.Exception(() => Act(attendance2));
+
+        // Asset
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<AlreadyParticipatingSameTimeException>();
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetAvailableDates))]
+    public void given_no_colliding_attendance_attend_should_succeed(DateTime from, DateTime to)
+    {
+        // Arrange
+        var attendance1 = new Attendance(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), _participant.Id, from, to);
+        var attendance2 = new Attendance(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), _participant.Id, GetDate(9, 30), GetDate(11));
+        _participant.Attend(attendance1);
+
+        // Act
+        Act(attendance2);
+
+        // Asset
+        _participant.Attendances.ShouldNotBeNull();
+        _participant.Attendances.ShouldContain(attendance2);
+    }
+
+    private readonly Participant _participant;
+
+    public Participant_Attend_Tests()
+    {
+        _participant = new Participant(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+    }
+    
+    private Guid _agendaItemId = Guid.NewGuid();
+
+    private static DateTime GetDate(int hour, int minute = 0, int second = 0)
+        => new (2024, 2, 6, hour, minute, second);
+    
+    
+    public static IEnumerable<object[]> GetCollidingDates()
+    {
+        yield return new object[] {GetDate(9), GetDate(10)};
+        yield return new object[] {GetDate(9), GetDate(11, 30)};
+        yield return new object[] {GetDate(10), GetDate(10, 30)};
+        yield return new object[] {GetDate(10), GetDate(12)};
+    }
+
+    public static IEnumerable<object[]> GetAvailableDates()
+    {
+        yield return new object[] {GetDate(8), GetDate(9)};
+        yield return new object[] {GetDate(8), GetDate(9, 30)};
+        yield return new object[] {GetDate(11), GetDate(12)};
+        yield return new object[] {GetDate(12), GetDate(13)};
+    }
+}
